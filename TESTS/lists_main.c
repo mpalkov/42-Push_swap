081bdc9e6858cee0b12d7e5d90c979cc6427ffe6
Martin Palkov <mpalkov@student.42barcelona.com>
2023-04-19 13:01:04 +0200

    progress backup stack_a working

--- TESTS/lists_main.c
+++ TESTS/lists_main.c
@@ -9,7 +9,7 @@ t_node	*ft_lst_getlast(t_node *stack)
 	return (stack);
 }
 
-int	ft_stadd_last(t_node *stack, int data)
+int	ft_stadd_last(t_node **stack, int data)
 {
 	t_node	*last;
 	t_node	*new;
@@ -18,23 +18,19 @@ int	ft_stadd_last(t_node *stack, int data)
 		new = malloc(sizeof (*new));
 	if (!new)
 		return (-1);
-	new.nbr = data;
-	new.next = NULL;
-	if (!stack)
-		stack = new;
+	new->nbr = data;
+	new->next = NULL;
+	last = ft_lst_getlast(*stack);
+	if (!last)
+	{
+		*stack = new;
 		return (1);
-	last = ft_lst_getlast(stack);
-
-	/*	if (!last)
-		ft_errmsg(ERR_NOSTACK);
-		FREE AND CLOSE;
-	*/
+	}
+	last->next = new;
 	return (1);
 }
 
-
-
-t_node	*init_a(int argc, char** argv)
+t_node	*ft_init_a(int argc, char** argv)
 {
 	t_node *stack;
 	int	i;
@@ -47,7 +43,7 @@ t_node	*init_a(int argc, char** argv)
 	while (i <= argc - 1)
 	{
 		nbr = atoi(argv[i++]);
-		if (ft_stadd_last(stack, nbr) == -1)
+		if (ft_stadd_last(&stack, nbr) == -1)
 			return (NULL);
 	}
 	return (stack);
@@ -70,19 +66,10 @@ int	main(void)
 	sta = NULL;
 	stb = NULL;
 
-	sta = ft_init_a();
-	if (!a)
+	sta = ft_init_a(argc, argv);
+	if (!sta)
 		return (-1);
 		// ERROR CREATING STACK A
 // CONTUNAR AQUI
-
-
-	
-	
-	a = malloc(sizeof(t_stack) * (argc - 1) + 1);
-	if (!a)
-		return (-1);
-
-
 	return (0);
 }
--- TESTS/push_swap.h
+++ TESTS/push_swap.h
@@ -1,4 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpalkov <mpalkov@student.42barcelo>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/19 12:05:29 by mpalkov           #+#    #+#             */
+/*   Updated: 2023/04/19 12:59:59 by mpalkov          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include <stdlib.h> //to use NULL
+
 typedef struct	s_node {
-	int		*nbr;
-	t_stack	*next;
+	int				nbr;
+	struct s_node	*next;
 }				t_node;
+
+#endif
